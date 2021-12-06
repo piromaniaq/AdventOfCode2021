3d1945e01d1a079eb7d7590a046e7416098b389c
Oskar Litka <piromaniaq@gmail.com>
2021-12-06 16:29:49 +0100

    Day 4 task 1

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -8,11 +8,28 @@ with open('day4.txt') as f:
 bingoNumbers = [int(x) for x in bingoInput[0].split(',')]
 bingoTable = []
 
-for i in range((len(bingoInput)//6)):
+for i in range((len(bingoInput) // 6)):
     temporaryTable = []
     for j in range(5):
-        temporaryTable.append([int(x) for x in bingoInput[(i*6)+2+j].split()])  # +2 because first two rows are useless
+        temporaryTable.append([int(x) for x in bingoInput[(i * 6) + 2 + j].split()])  # +2 because first two rows are useless
     bingoTable.append(temporaryTable)
 
-print(bingoTable)
+def checkNumber(table, number):
+    for x in table:
+        for y, verificatior in enumerate(line):
+            if verificatior == number:
+                line[y] = -1
+    return table
 
+def checkWin(table):
+    for row in table:
+        if sum(row) == -5:
+            return True
+        for x in range(5):
+            columnSum = 0
+            for y in range(5):
+                columnSum += table[x][y]
+            if columnSum == -5:
+                return True
+        else:
+            return False

bdffdaf21f6f0e58a0218cd340358686f59ac33d
Oskar Litka <piromaniaq@gmail.com>
2021-12-06 16:08:25 +0100

    Day 4 task 1

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -6,4 +6,13 @@ with open('day4.txt') as f:
         bingoInput.append(line.strip())
 
 bingoNumbers = [int(x) for x in bingoInput[0].split(',')]
-print(bingoNumbers)
+bingoTable = []
+
+for i in range((len(bingoInput)//6)):
+    temporaryTable = []
+    for j in range(5):
+        temporaryTable.append([int(x) for x in bingoInput[(i*6)+2+j].split()])  # +2 because first two rows are useless
+    bingoTable.append(temporaryTable)
+
+print(bingoTable)
+

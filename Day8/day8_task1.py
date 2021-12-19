48e04f7b4307d01b2ad590ab7d547de5bce28f2b
Oskar Litka <piromaniaq@gmail.com>
2021-12-19 20:28:25 +0100

    Day 8 task 1

--- Day8/day8_task1.py
+++ Day8/day8_task1.py
@@ -1,9 +1,16 @@
 def get_data(file):
     with open(file, 'r') as f:
+        count = 0
         data = f.read().splitlines()
-    return data
+        for line in data:
+            input, output = line.split(' | ')
+            output_split = output.split()
+            for x in output_split:
+                if len(x) in {2, 3, 4, 7}:
+                    count += 1
+    return count
 
 
 if __name__ == '__main__':
-    digits = get_data('test.txt')
+    digits = get_data('input.txt')
     print(digits)
